2dbf39b5c868b96780df2de4c5078f6580c3cbd1
Joseph Cohen <joseph.cohen@dinkbit.com>
2015-06-04 19:19:32 -0500

    Fixes on doc -> needs to make a repo to handle cache

--- app/Models/Doc.php
+++ app/Models/Doc.php
@@ -2,6 +2,7 @@
 
 namespace MXAbierto\Participa\Models;
 
+use Illuminate\Support\Facades\File;
 use Illuminate\Database\Eloquent\Collection;
 use Illuminate\Database\Eloquent\Model;
 use Illuminate\Database\Eloquent\SoftDeletes;
