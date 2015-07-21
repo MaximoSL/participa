08448f33d3fb3561ce5eb04b0802de5f830cc189
Joseph Cohen <joseph.cohen@dinkbit.com>
2015-07-21 15:02:50 -0500

    More CS Fixes

--- config/services.php
+++ config/services.php
@@ -24,33 +24,33 @@ return [
     ],
 
     'ses' => [
-        'key' => '',
+        'key'    => '',
         'secret' => '',
         'region' => 'us-east-1',
     ],
 
     'stripe' => [
         'model'  => 'MXAbierto\Participa\User',
-        'key' => '',
+        'key'    => '',
         'secret' => '',
     ],
 
     'facebook' => [
-        'client_id' => env('FB_CLIENT_ID'),
+        'client_id'     => env('FB_CLIENT_ID'),
         'client_secret' => env('FB_CLIENT_SECRET'),
-        'redirect' => env('APP_URL').'/auth/connect/facebook/callback',
+        'redirect'      => env('APP_URL').'/auth/connect/facebook/callback',
     ],
 
     'twitter' => [
-        'client_id' => env('TW_CLIENT_ID'),
+        'client_id'     => env('TW_CLIENT_ID'),
         'client_secret' => env('TW_CLIENT_SECRET'),
-        'redirect' => env('APP_URL').'/auth/connect/twitter/callback',
+        'redirect'      => env('APP_URL').'/auth/connect/twitter/callback',
     ],
 
     'linkedin' => [
-        'client_id' => env('LI_CLIENT_ID'),
+        'client_id'     => env('LI_CLIENT_ID'),
         'client_secret' => env('LI_CLIENT_SECRET'),
-        'redirect' => env('APP_URL').'/auth/connect/twitter/callback',
+        'redirect'      => env('APP_URL').'/auth/connect/twitter/callback',
     ],
 
 ];
